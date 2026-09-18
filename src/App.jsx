28d42732d98040b18f153bcdd78372801435bc2c
hadiatanveer08 <hadiatanveer59@gmail.com>
2026-09-19 03:15:14 +0500

    Create login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router'
 import Register from './Pages/Register'
+import Login from './Pages/Login'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<h1>HOME PAGE</h1>} />
         <Route path="/register" element={<Register />} />
+        <Route path="/Login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   )
