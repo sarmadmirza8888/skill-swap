21c30607571529f762dce90360c73fc3d65ec017
hadiatanveer08 <hadiatanveer59@gmail.com>
2026-09-19 02:57:24 +0500

    Create learner register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router'
+import Register from './Pages/Register'
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <h1>All good</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>HOME PAGE</h1>} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
