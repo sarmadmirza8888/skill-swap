8b67203ce1fff5dbee0a2d2a4956644b15693f31
hadiatanveer08 <hadiatanveer59@gmail.com>
2026-09-19 17:46:38 +0500

    Updated dashboard and resgister page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router'
 import Register from './Pages/Register'
 import Login from './Pages/Login'
+import Dashboard from './Pages/Dashboard'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<h1>HOME PAGE</h1>} />
         <Route path="/register" element={<Register />} />
         <Route path="/Login" element={<Login />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   )
